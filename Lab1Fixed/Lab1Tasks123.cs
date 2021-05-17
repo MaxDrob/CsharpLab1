d97860e18a1e5df4ee0cbc33f8cce23660b6088c
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-05-17 21:17:30 +0300

    Add project files.

---
+++ Lab1Fixed/Lab1Tasks123.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab1Tasks1abd3
+{
+       class Greeter
+
+    {
+        static void Main(string[] args)
+
+        {
+        Console.WriteLine("Please enter your name");
+        string myName = Console.ReadLine();
+        Console.WriteLine("Hello {0}", myName);
+         }
+
+    }
+}
---
+++ Lab1Task4Integer/Divider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divider
+{
+    class DivideIt
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input 1st integer:");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
+
+                Console.WriteLine("Input 2nd integer:");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
+
+                Console.WriteLine("Your digits: {0}, {1}", i, j);
+
+                int m = i / j;
+                Console.WriteLine("The result of dividing {0} / {1} = {2}", i, j, m);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e);
+            }
+            Console.Read();
+        }
+    }
+}
